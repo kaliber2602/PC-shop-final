eee49641d7befd2b8ffc78efa6c327057afe515b
NgoThuan <ngochithuan.dev@gmail.com>
2025-05-14 12:09:25 +0700

    add auto create database when import

--- backend/gearzone.sql
+++ backend/gearzone.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `gearzone`
 --
-
+CREATE DATABASE IF NOT EXISTS `gearzone` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gearzone`;
 -- --------------------------------------------------------
 
 --
